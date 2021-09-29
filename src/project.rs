bff28e151b81a70f87582cde677a93e1bf613fc8
VentGrey <omarpurataf@gmail.com>
2021-09-29 03:53:50 -0500

    Write basic functionality of init function

--- src/project.rs
+++ src/project.rs
@@ -1,3 +1,25 @@
+use scanrs::scanln;
+
 pub fn init() {
-    println!("Test");
+    println!("Starting new project, please select your desired options:");
+    println!(
+        "What kind of project would you like to setup?
+
+        1) C project (c17 - standard)
+        2) Python 3 project
+        3) Rust project
+
+Please enter your desired project in the line below (1, 2 or 3):
+");
+
+    let opt: String = scanln();
+
+    match opt.as_str() {
+        "1" => {},
+        "2" => {},
+        "3" => {},
+        _ => {}
+    }
+
+
 }
